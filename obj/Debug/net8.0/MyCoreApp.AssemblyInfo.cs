626e0f16cbd5e1a64df9f69eaa0484f59622d014
Donna Brar <dbrar25@my.centennialcollege.ca>
2025-01-31 17:55:09 -0500

    Added Fix for Issue #1 - update to welcome message; and added obj & properties directories to gitignore file

--- obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2cc584484831d64779656e7d3c591ed1edb03f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
